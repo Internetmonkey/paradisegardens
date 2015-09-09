9b4aeb91d4e453d2bce67be1d24454a5c5acf600
Alex James Bishop <alex@internetmonkey.com.au>
2015-09-09 19:09:46 +0930

    enable content and four block grid sections

--- template-home.php
+++ template-home.php
@@ -6,6 +6,6 @@
 
 <?php while (have_posts()) : the_post(); ?>
   <?php get_template_part('templates/page', 'hero'); ?>
-  <?php //get_template_part('templates/page', 'content'); ?>
-  <?php //get_template_part('templates/page', '4-block-grid'); ?>
+  <?php get_template_part('templates/page', 'content'); ?>
+  <?php get_template_part('templates/page', 'four-block-grid'); ?>
 <?php endwhile; ?>
