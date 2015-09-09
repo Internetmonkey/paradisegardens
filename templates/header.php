9ffa22ec54d35a5d619f2a7e2e535087a09c1e59
Alex James Bishop <alex@internetmonkey.com.au>
2015-09-09 19:02:57 +0930

    implement bootstrap nav walked class

--- templates/header.php
+++ templates/header.php
@@ -1,12 +1,39 @@
+<?php
+
+  use Roots\Sage\Nav;
+
+?>
+
 <header class="banner" role="banner">
+<nav class="navbar navbar-default navbar-fixed-top" role="navigation">
   <div class="container">
-    <a class="brand" href="<?= esc_url(home_url('/')); ?>"><?php bloginfo('name'); ?></a>
-    <nav role="navigation">
+    <div class="navbar-header">
+      <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".pg-nav-collapse">
+          <span class="sr-only">Toggle navigation</span>
+          <span class="icon-bar"></span>
+          <span class="icon-bar"></span>
+          <span class="icon-bar"></span>
+      </button>
+
+      <a class="navbar-brand" href="<?php bloginfo('url'); ?>">
+          <?php bloginfo('name'); ?>
+      </a>
+    </div>
+
       <?php
       if (has_nav_menu('primary_navigation')) :
-        wp_nav_menu(['theme_location' => 'primary_navigation', 'menu_class' => 'nav']);
+                wp_nav_menu( array(
+        'menu'              => 'primary_navigation',
+        'theme_location'    => 'primary_navigation',
+        'depth'             => 2,
+        'container'         => 'div',
+        'container_class'   => 'collapse navbar-collapse pg-nav-collapse',
+        'menu_class'        => 'nav navbar-nav',
+        'fallback_cb'       => 'wp_bootstrap_navwalker::fallback',
+        'walker'            => new Roots\Sage\Nav\BootstrapWalker(),)
+        );
       endif;
       ?>
-    </nav>
   </div>
+</nav>
 </header>
