16d5578e65ec306d9ebbfd3e6220cd93bfb2f334
Alex James Bishop <alex@internetmonkey.com.au>
2015-09-10 01:43:38 +0930

    enable masterplan template on relevant pages

--- template-explore.php
+++ template-explore.php
@@ -7,7 +7,7 @@
 <?php while (have_posts()) : the_post(); ?>
   <?php get_template_part('templates/page', 'hero'); ?>
   <?php get_template_part('templates/page', 'content'); ?>
-  <?php //get_template_part('templates/page', 'masterplan'); ?>
+  <?php get_template_part('templates/page', 'masterplan'); ?>
   <?php //get_template_part('templates/page', 'content-slider'); ?>
   <?php //get_template_part('templates/page', 'location'); ?>
 <?php endwhile; ?>
--- template-leasing.php
+++ template-leasing.php
@@ -7,6 +7,6 @@
 <?php while (have_posts()) : the_post(); ?>
   <?php get_template_part('templates/page', 'hero'); ?>
   <?php get_template_part('templates/page', 'content'); ?>
-  <?php //get_template_part('templates/page', 'masterplan'); ?>
+  <?php get_template_part('templates/page', 'masterplan'); ?>
   <?php //get_template_part('templates/page', 'content-slider'); ?>
 <?php endwhile; ?>
