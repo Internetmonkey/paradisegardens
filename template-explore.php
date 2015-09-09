644d8ce1199810a50b84b0db371b8c8f1c1d2c40
Alex James Bishop <alex@internetmonkey.com.au>
2015-09-09 13:27:26 +0930

    Change structure of custom page templates
    Include statements are commented out currently to avoid PHP fatals as the files do not exist yet.
    
    Deceisions made
    - Conditional / isset checks to be made in included files, not before
    - Still on the fence about explore vs leasing page as seperate templates

--- template-explore.php
+++ template-explore.php
@@ -5,6 +5,9 @@
 ?>
 
 <?php while (have_posts()) : the_post(); ?>
-  <?php get_template_part('templates/page', 'header'); ?>
-  <?php get_template_part('templates/content', 'page'); ?>
+  <?php //get_template_part('templates/page', 'hero'); ?>
+  <?php //get_template_part('templates/page', 'content'); ?>
+  <?php //get_template_part('templates/page', 'masterplan'); ?>
+  <?php //get_template_part('templates/page', 'content-slider'); ?>
+  <?php //get_template_part('templates/page', 'location'); ?>
 <?php endwhile; ?>
--- template-home.php
+++ template-home.php
@@ -5,6 +5,7 @@
 ?>
 
 <?php while (have_posts()) : the_post(); ?>
-  <?php get_template_part('templates/page', 'header'); ?>
-  <?php get_template_part('templates/content', 'page'); ?>
+  <?php //get_template_part('templates/page', 'hero'); ?>
+  <?php //get_template_part('templates/page', 'content'); ?>
+  <?php //get_template_part('templates/page', '4-block-grid'); ?>
 <?php endwhile; ?>
