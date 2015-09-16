efcef50562b736eaa5a7e52fca226b4fed99fb18
Geoff Irvine <mail@geoffirvine.com>
2015-09-16 19:54:58 +1000

    404 page layout

--- 404.php
+++ 404.php
@@ -1,7 +1,23 @@
+<section class="page-section">
+	<div class="container">
+		<div class="row">
+			<div class="col-md-10 col-md-offset-1 col-lg-8 col-lg-offset-2">
+
 <?php get_template_part('templates/page', 'header'); ?>
+
+
 
 <div class="alert alert-warning">
   <?php _e('Sorry, but the page you were trying to view does not exist.', 'sage'); ?>
 </div>
 
+
+
 <?php get_search_form(); ?>
+
+</div>
+</div>
+</div>
+
+</section>
+
